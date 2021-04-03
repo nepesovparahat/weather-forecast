e8d903a495eccff496aec9f771648415f48ca65f
Parahat <nepesovparahatt@gmail.com>
2021-04-03 15:39:13 +0300

    Updated css design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./App.css";
-import Home from "./components/Home";
+import MainWeather from './components/MainWeather';
+import WeatherBox from './components/WeatherBox';
 
 const App = () => {
   return (
-    <Home/>
+    <div className="App">
+    <header className="header-weather">
+        <MainWeather/> 
+    </header>
+    <div className="box-weather">  
+    <WeatherBox/>
+    <WeatherBox/>
+    <WeatherBox/>
+    <WeatherBox/>
+    <WeatherBox/>
+    <WeatherBox/>
+    </div>
+</div>
   )
 };
 
